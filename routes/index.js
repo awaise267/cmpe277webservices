3e284b16ba3e0dc151f23b3e0bec628355f4173e
Mohammad Awaise <awaise267@gmail.com>
2017-05-24 18:43:02 -0700

    changes to owner garages

--- routes/index.js
+++ routes/index.js
@@ -580,7 +580,7 @@ exports.get_user_history = function (req, res) {
 exports.owner_garages = function(req, res){
     var ownerId = req.body.id;
 
-    var query = "select * from Garage where ownerId = ?";
+    var query = "select g.id as id, g.name as name, g.lat as lat, g.long as `long`, g.spots as spots, g.availableSpots as availableSpots, g.rate as rate, g.ownerId as ownerId, sum(h.charges) as earnings from Garage g INNER JOIN giParkHistory h on g.id = h.garageId where g.ownerId = ?";
 
     var data = {};
     pool.getConnection(function (err, conn) {
